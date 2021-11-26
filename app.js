59b93ea225e1ade6d06f2335274631b6d5ca74a5
SridharIyappan <sridharsp4774@gmail.com>
2021-11-26 19:27:16 +0530

    after models creared

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import pkg from 'mongoose';
-const {NativeDate} = pkg;
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+//check
+const date = new Date();
+const cDate = date.getDate() + 3;
+const cMonth = date.getMonth() + 1;
+const cYear = date.getFullYear() + 2;
+
 app.use(cors());
 
 app.listen(port, () =>
-  console.log(`http://localhost:${port}, ${process.env.PORT}, ${NativeDate}`)
+  console.log(`http://localhost:${port}, ${process.env.PORT}, ${new Date(cYear, cMonth, cDate)}`)
 );
